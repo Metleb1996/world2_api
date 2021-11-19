4cc6eb4c980196a3fa884b808de5108445d628d9
Metleb1996 <metlebrustemov1996@gmail.com>
2021-11-19 15:21:04 +0400

    19.11.2021

--- index.php
+++ index.php
@@ -5,18 +5,34 @@ $jsonArray = array();
 $jsonArray["error"] = FALSE; 
 $_code = 200; 
 $_method = $_SERVER["REQUEST_METHOD"]; 
+function ser(){
+	print("Vareable: SERVER\n");
+	print_r($_SERVER);
+	print("Vareable: GET\n");
+	print_r($_GET);
+	print("Vareable: POST\n");
+	print_r($_POST);
+	print("Vareable: PUT\n");
+	print_r($_PUT);
+	print("Vareable: DELETE\n");
+	print_r($_DELETE);
+}
 switch($_method){
     case "GET":
-        print("GET");
+        print("Method: GET\n");
+	ser();
         break;
     case "POST":
-        print("POST");
+        print("Method: POST\n");
+	ser();
         break;
     case "PUT":
-        print("PUT");
+        print("Method: PUT\n");
+	ser();
         break;
     case "DELETE":
-        print("DELETE");
+        print("Method: DELETE\n");
+	ser();
         break;
 }
 ?>
