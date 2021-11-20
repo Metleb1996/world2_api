9f256eb5816439901f4a662531760728df96e767
Metleb1996 <metlebrustemov1996@gmail.com>
2021-11-20 21:04:51 +0400

    Renamed db_* variables to DB_*

--- db.php
+++ db.php
@@ -1,13 +1,10 @@
 <?php
-    require_once("config.php");
     try {
-        $db = new PDO('mysql:host='.$db_host.';dbname='.$db_name.';charset=utf8', $db_user, $db_password);
+        $db = new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8', $DB_USER, $DB_PASSWORD);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
         //die("MYSQL ERROR!");
     }
-    
-    define('APP_NAME', "W2_API");
 ?>
