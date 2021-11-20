c3ae55c8b89953a3e3b61dc12d187a9764e75ac4
Metleb1996 <metlebrustemov1996@gmail.com>
2021-11-20 21:51:20 +0400

    Added UserModel.php

---
+++ core/UserModel.php
@@ -0,0 +1,9 @@
+<?php
+require_once('AbstractModel.php');
+class UserModel extends AbstractModel{
+    public function get($in_data){}
+    public function post($in_data){}
+    public function patch($in_data){}
+    public function delete($in_data){}
+}
+?>
