fb2612eb7d0f4eca7c1d5e4710a299b892e5d00b
Metleb1996 <metlebrustemov1996@gmail.com>
2021-12-02 09:39:27 +0400

    Removed [db]

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
+    $db = NULL;
     try {
         $db = new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8', $DB_USER, $DB_PASSWORD);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $GLOBALS[$db] = $db;
     } catch (PDOException $e) {
         die("MYSQL ERROR!");
     }
