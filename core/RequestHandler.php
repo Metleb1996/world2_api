d2b078446edb536c2b3a6ea570284cceea06ba75
Metleb1996 <metlebrustemov1996@gmail.com>
2021-12-02 09:42:37 +0400

    Bug fixing

--- core/RequestHandler.php
+++ core/RequestHandler.php
@@ -1,4 +1,5 @@
 <?php
+require_once "Helper.php";
 class RequestHandler{
     private $_resources = NULL;
     private $_data = NULL;
@@ -13,22 +14,32 @@ class RequestHandler{
         $this->_resources[$resource->getUrl()] = $resource;
     }
     public function run(){
-        $url = $_SERVER['PATH_INFO'];
-        $res = $this->_resources[$url];
-        switch($_SERVER["REQUEST_METHOD"]){
-            case "GET":
-                
-                break;
-            case "POST":
-                
-                break;
-            case "PATCH":
-                
-                break;
-            case "DELETE":
-                
-                break;
+        if (array_key_exists('PATH_INFO', $_SERVER)){
+            $url = $_SERVER['PATH_INFO'];
+            if (array_key_exists($url, $this->_resources)){
+                $res = $this->_resources[$url];
+                switch($_SERVER["REQUEST_METHOD"]){
+                    case "GET":
+                        $res_data = $res->get($this->_data);
+                        break;
+                    case "POST":
+                        $res_data = $res->post($this->_data);
+                        break;
+                    case "PATCH":
+                        $res_data = $res->patch($this->_data);
+                        break;
+                    case "DELETE":
+                        $res_data = $res->delete($this->_data);
+                        break;
+                }
+                $this->_jsonArray['_code'] = $res_data['_code'];
+                $this->_jsonArray['_data'] = $res_data['_data'];
+                return $this->_jsonArray;
+            }
         }
+        $this->_jsonArray['_code'] = 404;
+        $this->_jsonArray['_data'] = Helper::HttpStatus(404);
+        return $this->_jsonArray;
     }
 }
 ?>
